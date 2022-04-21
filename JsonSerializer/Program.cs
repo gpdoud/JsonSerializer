f7deb4aee20d9ce74b1da2361decc7af3d66478a
Greg Doud <gdoud@gmail.com>
2022-04-20 22:55:04 -0400

    Committed by GitUtil

--- JsonSerializer/Program.cs
+++ JsonSerializer/Program.cs
@@ -1,23 +1,19 @@
 ﻿using JsonSerializer;
 
-var options = new System.Text.Json.JsonSerializerOptions()
-{
+var options = new System.Text.Json.JsonSerializerOptions() {
     PropertyNameCaseInsensitive = true,
     WriteIndented = true
 };
 
-Wrapper wrapper = new()
-{
-    TransactionResponse = new()
-    {
+Wrapper wrapper = new() {
+    TransactionResponse = new() {
         StatusCode = 200,
         HasError = false,
         StatusMessage = "Event Message(s) Received",
         ErrorMessage = null,
         HttpStatusCode = 0,
         SubmittedTransactions = new List<Transaction>() {
-            new Transaction()
-            {
+            new Transaction() {
                 ConfirmationNumber = "something"
             }
         },
@@ -31,6 +27,6 @@ Wrapper wrapper = new()
 var jsonTr = System.Text.Json.JsonSerializer.Serialize<Wrapper>(wrapper, options);
 
 // deserialize json back to customer instance
-//var tr = System.Text.Json.JsonSerializer.Deserialize<TransactionResponse>(jsonTr);
+var tr = System.Text.Json.JsonSerializer.Deserialize<Wrapper>(jsonTr);
 
 Console.WriteLine(jsonTr);
